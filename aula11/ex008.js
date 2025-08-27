846a556bffbd8279262a29164130f24deee407bf
kelvycaua <kelvycauapc@gmail.com>
2025-08-27 20:38:43 -0300

    new

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 79
+console.log(`A velocidade do seu carro é ${vel} por km/h`)
+
+if (vel > 60 ) { //condição simples
+    console.log('Você utrapassou a velocidade permitida, MULTADO')
+}
+
+console.log('Use cinto de segurança')
